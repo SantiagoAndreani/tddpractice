ecde6cc000e41c28d18f92cba8fa52f43b7d6ddc
Santiago Andreani <santia.89@gmail.com>
2020-09-03 16:05:26 -0300

    first

---
+++ src/main/java/tdd/practice/Dollar.java
@@ -0,0 +1,14 @@
+package tdd.practice;
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+
+    }
+
+    void times(int multiplier) {
+
+    }
+}
--- src/main/java/tdd/practice/Main.java
+++ src/main/java/tdd/practice/Main.java
@@ -3,6 +3,6 @@ package tdd.practice;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+
     }
 }
