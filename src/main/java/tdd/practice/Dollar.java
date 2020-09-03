8b7b89ebf4210c2315d914bf509492b308bf1361
Santiago Andreani <santia.89@gmail.com>
2020-09-03 16:10:27 -0300

    .

--- src/main/java/tdd/practice/Dollar.java
+++ src/main/java/tdd/practice/Dollar.java
@@ -5,10 +5,10 @@ public class Dollar {
     int amount;
 
     public Dollar(int amount) {
-
+        this.amount = amount;
     }
 
     void times(int multiplier) {
-
+        amount *= multiplier;
     }
 }
