e0be35a1445adbc8f5d69eaae11da82fde42f107
Santiago Andreani <santia.89@gmail.com>
2020-09-04 01:02:40 -0300

    completecircule

--- src/test/java/tdd/practice/MoneyTest.java
+++ src/test/java/tdd/practice/MoneyTest.java
@@ -85,4 +85,36 @@ public class MoneyTest {
         assertEquals(1, new Bank().rate("CHF", "CHF"));
         assertEquals(1, new Bank().rate("CHF", "CHF"));
     }
+
+    @Test
+    public void textMixedAddition() {
+        Expression fivebucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fivebucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenfrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF","USD", 2);
+        Expression sum = new Sum(fiveBucks, tenfrancs.plus(fiveBucks));
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+        Money result = bank.reduce(sum,"USD");
+        assertEquals(Money.dollar(20), result);
+    }
 }
